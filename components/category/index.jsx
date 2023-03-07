47f574b34305cffdf0e8a6f6cb7c855bd4ae1d76
cemtatli <cemtatli3535@gmail.com>
2023-03-07 17:21:27 +0300

    flowbite componentleri eklendi

--- components/category/index.jsx
+++ components/category/index.jsx
@@ -5,48 +5,12 @@ import Link from "next/link";
 export default function Category() {
   return (
     <div className="flex items-center justify-center gap-2.5 px-4 py-2 md:gap-5 flex-wrap my-4 sm:flex-nowrap flex-col sm:flex-row w-full">
-      <Link
-        href={"/html"}
-        className="flex md:h-10 h-8 w-full items-center justify-center rounded-full bg-gradient-to-l font-medium text-sm lg:text-base  from-orange-400 to-orange-500 px-3 py-1 text-white transition-all duration-75 hover:shadow-md hover:ring-2 hover:ring-orange-600 sm:w-40"
-      >
-        HTML
-       
-      </Link>
-      <Link
-        href={"/css"}
-        className="flex lg:text-base  md:h-10 h-8 w-full items-center justify-center rounded-full bg-gradient-to-l font-medium text-sm  from-sky-400 to-blue-500 px-3 py-1 text-white  transition-all duration-75 hover:shadow-md hover:ring-2 hover:ring-blue-600 sm:w-40"
-      >
-        CSS
-       
-      </Link>
-      <Link
-        href={"/javascript"}
-        className="md:h-10 h-8 lg:text-base w-full flex items-center justify-center rounded-full bg-gradient-to-l font-medium text-sm  from-bright-sun-400 to-bright-sun-500 px-3 py-1 text-white  transition-all duration-75 hover:shadow-md hover:ring-2 hover:ring-bright-sun-600 sm:w-40"
-      >
-        JavaScript
-
-      </Link>
-      <Link
-        href={"/react"}
-        className="flex lg:text-base md:h-10 h-8 w-full items-center justify-center rounded-full bg-gradient-to-l font-medium text-sm  from-ebony-400 to-ebony-500 px-3 py-1 text-white  transition-all duration-75 hover:shadow-md hover:ring-2 hover:ring-ebony-600 sm:w-40"
-      >
-        React JS
-
-      </Link>
-      <Link
-        href={"/nextjs"}
-        className="flex lg:text-base md:h-10 h-8 w-full items-center justify-center rounded-full bg-gradient-to-l font-medium text-sm  from-slate-400 to-slate-500 px-3 py-1 text-white  transition-all duration-75 hover:shadow-md hover:ring-2 hover:ring-slate-600 sm:w-40"
-      >
-        Next JS
-     
-      </Link>
-      <Link
-        href={"/git"}
-        className="flex lg:text-base md:h-10 h-8 w-full items-center justify-center rounded-full bg-gradient-to-l font-medium text-sm  from-flamingo-400 to-flamingo-500 px-3 py-1 text-white  transition-all duration-75 hover:shadow-md hover:ring-2 hover:ring-flamingo-600 sm:w-40"
-      >
-        Git
-      </Link>
-
+    <Link href={'/html'} class="text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300 dark:focus:ring-orange-800 font-medium rounded-full text-sm px-5 py-2.5 text-center flex items-center justify-center w-full sm:w-40">HTML</Link>
+    <Link href={'/css'} class="text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center flex items-center justify-center w-full sm:w-40">CSS</Link>
+    <Link href={'/javascript'} class="text-white bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-800 font-medium rounded-full text-sm px-5 py-2.5 text-center flex items-center justify-center w-full sm:w-40">JavaScript</Link>
+    <Link href={'/'} class="text-white bg-gradient-to-r from-indigo-400 via-indigo-500 to-indigo-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:indigo-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center flex items-center justify-center w-full sm:w-40">React</Link>
+    <Link href={'/'} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center flex items-center justify-center w-full sm:w-40">Vue JS</Link>
+    <Link href={'/'} class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-full text-sm px-5 py-2.5 text-center flex items-center justify-center w-full sm:w-40">Git</Link>
     </div>
   );
 }
--- components/tooltip/index.jsx
+++ components/tooltip/index.jsx
@@ -6,27 +6,26 @@ import { XMarkIcon, ArrowSmallRightIcon } from "@heroicons/react/24/solid";
 
 export default function Tooltip() {
   return (
-    <div className="tooltip group relative flex w-full flex-col items-start justify-center gap-2.5 bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 px-4 py-2 text-sm text-ebony-900 sm:items-center md:flex-row md:gap-5">
+    <div className="tooltip group relative flex w-full flex-col items-start justify-center gap-2.5 bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 px-4 py-2 md:py-3 text-sm text-ebony-900 sm:items-center md:flex-row md:gap-5">
       <span>
         Açık kaynaklı projemize katkıda bulunmak isterseniz, GitHub üzerinden ulaşabilirsiniz.
       </span>
       <Link
         target="_blank"
         href="https://github.com/cemtatli/sozluk.dev"
-        class="group flex-none rounded-full bg-ebony-900 py-1 px-3 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
       >
-        Repo'ya git
-        <span aria-hidden="true" className="ml-1">
-          →
-        </span>
+        <span aria-hidden='true' className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-3 py-1 rounded-full dark:bg-blue-900 dark:text-blue-300">Repo'ya git →</span>
       </Link>
       <XMarkIcon
         onClick={() => {
           document.querySelector(".tooltip").classList.toggle("hidden");
         }}
         aria-hidden="true"
-        className="absolute right-2.5 bottom-2 h-4.5 w-4.5 shrink-0 cursor-pointer rounded-full text-gray-700 transition  duration-100  hover:scale-110 hover:text-gray-900 hover:ring-1 hover:ring-gray-900 hover:ring-offset-gray-900 sm:bottom-3.5"
+        className="absolute right-2.5 bottom-2.5 h-4 w-4 shrink-0 cursor-pointer rounded-full text-gray-700 transition  duration-100  hover:scale-110 hover:text-gray-900 hover:ring-1 hover:ring-gray-900 hover:ring-offset-gray-900 md:bottom-3.5 "
       />
+      
     </div>
+
+
   );
 }
